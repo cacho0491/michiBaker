1b9e41744b77b6f100b628a811a9030711ce1f91
Carlos Correa <carloscorrea91@hotmail.com>
2021-01-05 11:39:17 +0000

    Recipe Template

--- src/templates/recipe-page.js
+++ src/templates/recipe-page.js
@@ -6,6 +6,73 @@ import Layout from "../components/Layout";
 import styles from "./recipe.module.css";
 import photo from "../../public/img/products-grid1.jpg";
 
+export const RecipePageTemplate = ({
+  title,
+  description,
+  ingredients,
+  steps,
+}) => {
+  <section className={styles.recipe}>
+    <div className={styles.recipeTop}>
+      <img src={photo} />
+      <div className={styles.recipeInfo}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+    <div className={styles.recipeContent}>
+      <div className={styles.instructions}>
+        <h3>Instructions</h3>
+        <div className={styles.recipeSteps}>
+          <ul>
+            {steps.map((step, index) => {
+              return (
+                <li>
+                  <h5>Step {index + 1}</h5>
+                  <p>{step}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className={styles.ingredients}>
+        <h3>Ingredients</h3>
+        <div>
+          <ul>
+            {ingredients.map((ingredient) => {
+              return (
+                <li>
+                  <p>{ingredient}</p>
+                </li>
+              );
+            })}
+
+            <hr />
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>;
+};
+
+const RecipePost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <RecipePageTemplate
+        title={post.title}
+        description={post.description}
+        ingredients={post.ingredients}
+        steps={post.steps}
+      />
+    </Layout>
+  );
+};
+
+export default RecipePost;
+
 export const query = graphql`
   query($id: String!) {
     markdownRemark(id: { eq: $id }) {
@@ -18,58 +85,3 @@ export const query = graphql`
     }
   }
 `;
-
-const RecipePost = (props) => {
-  return (
-    <Layout>
-      <section className={styles.recipe}>
-        <div className={styles.recipeTop}>
-          <img src={photo} />
-          <div className={styles.recipeInfo}>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.description}</p>
-          </div>
-        </div>
-        <div className={styles.recipeContent}>
-          <div className={styles.instructions}>
-            <h3>Instructions</h3>
-            <div className={styles.recipeSteps}>
-              <ul>
-                {props.data.markdownRemark.frontmatter.steps.map(
-                  (step, index) => {
-                    return (
-                      <li>
-                        <h5>Step {index + 1}</h5>
-                        <p>{step}</p>
-                      </li>
-                    );
-                  }
-                )}
-              </ul>
-            </div>
-          </div>
-          <div className={styles.ingredients}>
-            <h3>Ingredients</h3>
-            <div>
-              <ul>
-                {props.data.markdownRemark.frontmatter.ingredients.map(
-                  (ingredient) => {
-                    return (
-                      <li>
-                        <p>{ingredient}</p>
-                      </li>
-                    );
-                  }
-                )}
-
-                <hr />
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
-  );
-};
-
-export default RecipePost;
